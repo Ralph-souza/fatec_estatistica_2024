4062ea53ebf7d05df882ff8acb83653371494e7a
codesaur-course <codesaurcourse@gmail.com>
2024-06-21 13:36:44 -0300

    Add get_probability function

--- calculo/views.py
+++ calculo/views.py
@@ -13,12 +13,24 @@ def get_variancy(value_a, value_b):
     return ((value_b - value_a) ** 2) / 12
 
 
-def standard_deviation(value_a, value_b):
+def get_standard_deviation(value_a, value_b):
     return math.sqrt(get_variancy(value_a, value_b))
 
 
-def coefficient_variation(value_a, value_b):
-    return (100 * standard_deviation(value_a, value_b)) / get_average(value_a, value_b)
+def get_coefficient_variation(value_a, value_b):
+    return (100 * get_standard_deviation(value_a, value_b)) / get_average(value_a, value_b)
+
+
+def get_probability(value_a, value_b, value_c, value_d):
+    if value_a == value_c:
+        probability = (value_c - value_a) / (value_b - value_a)
+        return f"P(x < {value_c}) = {probability:.2f}%"
+    elif value_b == value_d:
+        probability = (value_b - value_d) / (value_b - value_a)
+        return f"P(x > {value_d}) = {probability:.2f}%"
+    else:
+        probability = (100 * (value_d - value_c)) / (value_b - value_a)
+        return f"P({value_c} <= x <= {value_d}) = {probability:.2f}%"
 
 
 def calculate(request):
@@ -41,12 +53,10 @@ def calculate(request):
 
         result['average'] = f"{get_average(value_a, value_b):.2f}"
         result['variancy'] = f"{get_variancy(value_a, value_b):.4f}"
-        result['standard_deviation'] = f"{standard_deviation(value_a, value_b):.4f}"
-        result['coefficient_variation'] = f"{coefficient_variation(value_a, value_b):.2f}%"
+        result['standard_deviation'] = f"{get_standard_deviation(value_a, value_b):.4f}"
+        result['coefficient_variation'] = f"{get_coefficient_variation(value_a, value_b):.2f}%"
 
         if value_c is not None and value_d is not None:
-            pass
-
-            result['probability'] = f'Probabilidade de C={value_c} e D={value_d}'
+            result['probability'] = get_probability(value_a, value_b, value_c, value_d)
 
     return render(request, 'calculate.html', {'form': form, 'result': result})
